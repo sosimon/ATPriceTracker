afb3003c33e25d040815b0e7ef62ea43e7799d1c
Simon So <simon.so@gmail.com>
2017-12-10 19:43:23 -0800

    add deploy.sh

--- fetchData/deploy.sh
+++ fetchData/deploy.sh
@@ -2,12 +2,12 @@
 
 aws lambda create-function \
     --region us-west-2 \
+    --profile default \
     --function-name fetchData \
-    --zip-file file://home/ec2-user/fetchData.zip \
+    --zip-file fileb://fetchData.zip \
     --role arn:aws:iam::490069154287:role/lambda-execution-role \
     --handler fetchData.lambda_handler \
     --runtime python3.6 \
-    --profile adminuser \
     --timeout 10 \
     --memory-size 1024
 
