8db3f70312dfb9c48c8d49c27e660a21d0785de2
Simon So <simon.so@gmail.com>
2017-12-16 22:21:32 -0800

    add site

---
+++ site/app.py
@@ -0,0 +1,44 @@
+# app.py
+
+from flask import Flask, g, render_template
+import psycopg2
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+def connect_db():
+    conn = None
+    params = "dbname='{}' user='{}' host='{}' password='{}'".format(
+            app.config["DB_NAME"],
+            app.config["DB_USER"],
+            app.config["DB_HOST"],
+            app.config["DB_PASS"])
+    try:
+        conn = psycopg2.connect(params)
+        print("INFO: Connected to database!")
+    except:
+        print("ERROR: Could not connect to the database!")
+    return conn
+
+def get_db_conn():
+    if not hasattr(g, "pg_db"):
+        g.pg_db = connect_db()
+    return g.pg_db
+
+@app.teardown_appcontext
+def close_db(exception):
+    if hasattr(g, 'pg_db'):
+        g.pg_db.close()
+
+@app.route("/")
+def main():
+    conn = get_db_conn()
+    cur = conn.cursor()
+    cur.execute("SELECT * from price;")
+    data = cur.fetchall()
+    print("INFO: {} rows fetched".format(cur.rowcount))
+    cur.close()
+    return render_template("index.html", data=data)
+
+if __name__=="__main__":
+    app.run()
---
+++ site/config.py
@@ -0,0 +1,8 @@
+# config.py
+
+DEBUG = True
+DB_NAME = "autotrader"
+DB_HOST = "autotrader.cuicpsblup3n.us-west-2.rds.amazonaws.com"
+#DB_HOST = "localhost"
+DB_USER = "administrator"
+DB_PASS = "secretpassword"
